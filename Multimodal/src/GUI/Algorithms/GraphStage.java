17e0b02608baafdf6adb4c707d117b37381ca426
Thomas Bourne <thomas.bourne@ymail.com>
2013-01-20 07:52:32 +0000

    Updated New Java Files
    Will now be switching to another branch to work on intergrating the new
    structure into the program

--- Multimodal/src/GUI/Algorithms/GraphStage.java
+++ Multimodal/src/GUI/Algorithms/GraphStage.java
@@ -16,10 +16,17 @@ import java.util.LinkedList;
 public class GraphStage {
     private Graph graph = new Graph();
 
-    LinkedList<GraphNode> VisitedNodes; // list of all visited nodes, these will be coloured blue
-    LinkedList<Edge> VisitedEdges;      // list of all visited edges, these will also be blue
-    GraphNode ActiveNode;               // the currently active node, this will be yellow
-    Edge ActiveEdge;                    // the currently actuve edge, this will be yellow as well
+    private LinkedList<GraphNode> VisitedNodes; // list of all visited nodes, these will be coloured blue
+    private LinkedList<Edge> VisitedEdges;      // list of all visited edges, these will also be blue
+    private GraphNode ActiveNode;               // the currently active node, this will be yellow
+    private Edge ActiveEdge;                    // the currently actuve edge, this will be yellow as well
+
+    private String QueueText;                   // Holds info on which nodes are curretnly in the queue
+    private String InfoText;                    // tells the user what is currently being looked at by the algorithm
+
+    public GraphStage(Graph graph){
+        this.graph = graph;
+    }
 
     /**
      * @return the graph
@@ -33,6 +40,90 @@ public class GraphStage {
      */
     public void setGraph(Graph graph) {
         this.graph = graph;
+    }
+
+    /**
+     * @return the VisitedNodes
+     */
+    public LinkedList<GraphNode> getVisitedNodes() {
+        return VisitedNodes;
+    }
+
+    /**
+     * @param VisitedNodes the VisitedNodes to set
+     */
+    public void setVisitedNodes(LinkedList<GraphNode> VisitedNodes) {
+        this.VisitedNodes = VisitedNodes;
+    }
+
+    /**
+     * @return the VisitedEdges
+     */
+    public LinkedList<Edge> getVisitedEdges() {
+        return VisitedEdges;
+    }
+
+    /**
+     * @param VisitedEdges the VisitedEdges to set
+     */
+    public void setVisitedEdges(LinkedList<Edge> VisitedEdges) {
+        this.VisitedEdges = VisitedEdges;
+    }
+
+    /**
+     * @return the ActiveNode
+     */
+    public GraphNode getActiveNode() {
+        return ActiveNode;
+    }
+
+    /**
+     * @param ActiveNode the ActiveNode to set
+     */
+    public void setActiveNode(GraphNode ActiveNode) {
+        this.ActiveNode = ActiveNode;
+    }
+
+    /**
+     * @return the ActiveEdge
+     */
+    public Edge getActiveEdge() {
+        return ActiveEdge;
+    }
+
+    /**
+     * @param ActiveEdge the ActiveEdge to set
+     */
+    public void setActiveEdge(Edge ActiveEdge) {
+        this.ActiveEdge = ActiveEdge;
+    }
+
+    /**
+     * @return the QueueText
+     */
+    public String getQueueText() {
+        return QueueText;
+    }
+
+    /**
+     * @param QueueText the QueueText to set
+     */
+    public void setQueueText(String QueueText) {
+        this.QueueText = QueueText;
+    }
+
+    /**
+     * @return the InfoText
+     */
+    public String getInfoText() {
+        return InfoText;
+    }
+
+    /**
+     * @param InfoText the InfoText to set
+     */
+    public void setInfoText(String InfoText) {
+        this.InfoText = InfoText;
     }
 
 }
--- Multimodal/src/GUI/Algorithms/ProcessedGraph.java
+++ Multimodal/src/GUI/Algorithms/ProcessedGraph.java
@@ -4,6 +4,9 @@
  */
 package GUI.Algorithms;
 
+import GUI.Edge;
+import GUI.Graph;
+import GUI.GraphNode;
 import java.util.LinkedList;
 
 /**
@@ -14,6 +17,41 @@ public class ProcessedGraph {
     private LinkedList<GraphStage> graph = new LinkedList<>();
 
 
+    public ProcessedGraph(){
+
+    }
+
+    /**
+     * Adds a new GraphStage onto the end of the list
+     * @param newStage - the overall graph
+     * @param activeNode - the node which is currently being looked at
+     * @param activeEdge - the edge which is currently being looked at
+     * @param visitedNodes - a list of nodes that have already been visited
+     * @param visitedEdges - a list of edges that have already been visited
+     * @param queueText - the current priority queue for this graph
+     * @param infoText - just stuff about what is currently happening
+     * @return true on completion
+     */
+    public boolean addStage(
+            Graph newStage,
+            GraphNode activeNode,
+            Edge activeEdge,
+            LinkedList<GraphNode> visitedNodes,
+            LinkedList<Edge> visitedEdges,
+            String queueText,
+            String infoText
+            ){
+
+        GraphStage temp = new GraphStage(newStage);
+        temp.setActiveNode(activeNode);
+        temp.setActiveEdge(activeEdge);
+        temp.setVisitedEdges(visitedEdges);
+        temp.setVisitedNodes(visitedNodes);
+        temp.setInfoText(infoText);
+        temp.setQueueText(queueText);
+        graph.addLast(temp);
+        return true;
+    }
 
     /**
      * @return the graph
